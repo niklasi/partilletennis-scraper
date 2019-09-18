430936b6615dfcdcabd2955e48064662ef6b1a81
Niklas Ingholt <niklas@ingholt.com>
2019-09-18 16:30:11 +0200

    Add external css

--- app.rb
+++ app.rb
@@ -4,6 +4,7 @@ require './scraper'
 
 set :port, 8080
 set :haml, :format => :html5
+set :public_folder, File.dirname(__FILE__) + '/static'
 
 get '/foretagstennis/:series' do
   haml :index, :locals => foretagstennis(params[:series])
