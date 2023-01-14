8dd0282c4725d0f4881d5bdccfe795f6cbf99d63
Niklas Ingholt <niklas@ingholt.com>
2023-01-14 01:03:38 +0100

    fix: hard coded series data since website is abandoned

--- src/app.rb
+++ src/app.rb
@@ -1,17 +1,18 @@
 require 'sinatra'
 require 'sinatra/reloader' if development?
-require 'json'
-require './scraperTeamSeries'
-require './scraperExcerciseSeries'
+# require 'json'
+# require './scraperTeamSeries'
+# require './scraperExcerciseSeries'
 
 set :port, 8080
 
 get '/scraper/lagserien/:series', :provides => 'json' do
-  series = TeamSeries.load(params[:series])
-  series.to_json
+  # series = TeamSeries.load(params[:series])
+  # series.to_json
 end
 
 get '/scraper/motionserier/:series', :provides => 'json' do
-  series = ExcerciseSeries.load(params[:series])
-  series.to_json
+  # series = ExcerciseSeries.load(params[:series])
+  # series.to_json
+  File.read(params[:series] + '.json')
 end
